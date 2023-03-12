7b86de172c670b6eda40467679745d014122a699
Enzo Belém <enzogt5000@gmail.com>
2023-03-12 01:25:01 -0300

    views returning 404 in wrong fecths/ empty note pages created

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from .models import Note
+from .models import Note, Tag
 
 # from utils.notes.factory import make_note
 
@@ -9,19 +9,31 @@ def home(request):
     notes = Note.objects.filter(is_published=True).order_by('-id')
     return render(request, 'notes/pages/home.html', context={
         'notes': notes,
+        'title': 'Home'
     })
 
 
 def notes_by_tag(request, tag_id):
-    notes = Note.objects.filter(
-        is_published=True, tags__id=tag_id).order_by('-id')
+    notes = get_list_or_404(
+        Note.objects.filter(
+            is_published=True,
+            tags__id=tag_id
+        ).order_by('-id')
+    )
+    tag_name = get_object_or_404(Tag, id=tag_id).name
+
     return render(request, 'notes/pages/home.html', context={
         'notes': notes,
+        'title': f'Tag - {tag_name}'
     })
 
 
 def note(request, note_id):
-    note = Note.objects.get(id=note_id)
+    note = get_object_or_404(
+        Note.objects.filter(
+            id=note_id
+        ).order_by('-id')
+    )
     return render(request, 'notes/pages/note_page.html', context={
         'note': note,
     })
